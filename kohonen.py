5d1144d6db784b2d67927e2458224729806aaa3a
agrena <agren.alexander@gmail.com>
2016-10-09 23:00:30 +0200

    m

--- kohonen.py
+++ kohonen.py
@@ -5,12 +5,13 @@ from data import triangle_data
 # Init weights
 nr_of_input_units = 2
 nr_of_output_units = 100
-W_shape = (nr_of_output_units,nr_of_input_units)
+W_shape = (nr_of_output_units, nr_of_input_units)
 W = np.random.normal(loc=0.5, scale=0.01, size=W_shape)
+
 plt.figure()
 plt.scatter(W[:, 0], W[:, 1], color='black', marker='*')
-plt.ylim(ymin=0.0)
-plt.xlim(xmin=0.0)
+plt.ylim(ymin=0.0,ymax=1.0)
+plt.xlim(xmin=0.0,xmax=1.0)
 plt.show()
 
 #neighbourhood = lambda t, i1, i0: np.exp(-((r[i1]-r[i0])**2)/(2*sigma(t)**2))
@@ -28,7 +29,7 @@ T_order = 1000
 for _ in range(T_order):
     x = triangle_data[np.random.choice(range(len(triangle_data)), None)]
     i0 = np.argmax(W.dot(x))
-    #neighbourhood = lambda t, i1, i0: np.exp(-((r[i1] - r[i0]) ** 2) / (2 * sigma(t) ** 2))
+    #neighbourhood = lambda t, i1, i0: np.exp(-(np.linalg.norm(W - W[i0])**2) / (2 * sigma(t) ** 2))
     #dW = n() * neighbourhood() * ()
     #W = W + dW
     break
